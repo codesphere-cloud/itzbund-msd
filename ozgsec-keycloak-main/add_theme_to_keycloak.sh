7eae9911264b8c564c3aa8b77123f7ec04796f4c
jan-david.wiederstein <jan-david.wiederstein@codesphere.com>
2024-11-11 17:18:41 +0000

    changes

--- ozgsec-keycloak-main/add_theme_to_keycloak.sh
+++ ozgsec-keycloak-main/add_theme_to_keycloak.sh
@@ -9,8 +9,8 @@ fi
 latest_tag=$1
 
 keycloak_dir="./keycloak-${latest_tag}/themes"
-source_email="/home/user/app/azgsec-keycloak-main/build_keycloak/src/main/resources/themes/ozgsec-keycloak-theme/email"
-source_login="/home/user/app/azgsec-keycloak-main/build_keycloak/src/main/resources/themes/ozgsec-keycloak-theme/login"
+source_email="/home/user/app/ozgsec-keycloak-main/build_keycloak/src/main/resources/theme/ozgsec-keycloak-theme/email"
+source_login="/home/user/app/ozgsec-keycloak-main/build_keycloak/src/main/resources/theme/ozgsec-keycloak-theme/login"
 
 mkdir -p "$keycloak_dir"
 cp -r "$source_email" "$keycloak_dir"
